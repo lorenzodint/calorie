51dd16c230ac73a71be8b8f9fde949600dffeeea
Lorenzo D'intino <64317922+lorenzodint@users.noreply.github.com>
2025-04-14 14:48:27 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -10,6 +10,13 @@ async def app():
         session = st.session_state
         st.set_page_config(layout="centered")
 
+        hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+        st.markdown(hide_github_icon, unsafe_allow_html=True)
+
         configurazione_session = await config_session()
         if not configurazione_session.stato:
             return st.error(configurazione_session.errore)
@@ -19,18 +26,9 @@ async def app():
         router = await func.router_pagine()
         if not router.stato:
             return st.error(router.errore)
-        
-        
-        
-        
-        
+
         # st.write(session)
-        
+
         return Result()
     except Exception as e:
         return Result(stato=False, errore="Errore caricamento App")
-    
-    
-    
-
-    
